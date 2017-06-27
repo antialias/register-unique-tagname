e142acc6a57b7e114e2a27f6bf14dfc7c326dec2
Thomas Hallock <thomas.hallock@mixpanel.com>
2017-06-27 11:55:43 -0700

    exporting makeTagName

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function makeTagName(tagPrefix, unique) {
   return `${tagPrefix}-${hash(unique)}`;
 };
 module.exports = {
+  makeTagName: makeTagName,
   registerElement: function (tagPrefix, constructor, unique) {
     var tagName = makeTagName(tagPrefix, unique)
     constructor.tagName = tagName;
